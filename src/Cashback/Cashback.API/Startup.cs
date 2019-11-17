c8480adcefa31a9a79312c7d8dd3bcf97a635a68
Carlos Henrique Prado Sousa <chpsousa@hotmail.com>
2019-11-17 18:33:37 -0300

    inicializando o projeto da api

---
+++ src/Cashback/Cashback.API/Startup.cs
@@ -0,0 +1,85 @@
+using Cashback.Domain;
+using Cashback.Domain.Commands;
+using Cashback.Domain.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Cashback.API
+{
+    public class Startup
+    {
+        public static string Environment { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CashbackDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("AuditCommands"))
+                .EnableSensitiveDataLogging()
+                .ConfigureWarnings(warnings => warnings
+                    .Throw(CoreEventId.IncludeIgnoredWarning)
+                    .Throw(RelationalEventId.QueryClientEvaluationWarning)));
+
+            services.AddScoped<CashbackCommandsHandler>();
+            //services.AddScoped<CashbackQueriesHandler>();
+            services.AddScoped<EventsHandler>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info() { Title = "Cashback API", Version = "v1" });
+            });
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            Environment = env.EnvironmentName;
+            CashbackStartup.Configure(Configuration.GetConnectionString("CashbackDb"));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Qualiex Audit API.v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            // Enabling Cross-Origin Requests
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
